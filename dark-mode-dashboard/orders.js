b7a6265c523aebd49c08501d000f6238787c8cbd
Eduardo Carvalho Cardoso <dxcar10000@hotmail.com>
2023-07-26 23:30:18 -0300

    Adicionando corpo do projeto dark-mode-dashboard

---
+++ dark-mode-dashboard/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Playstation 5',
+        productNumber: '1',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'Notebook Dell G15',
+        productNumber: '2',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'Iphone 15',
+        productNumber: '3',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    }
+]
